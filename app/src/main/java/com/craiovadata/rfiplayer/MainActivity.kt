7bad0dfa2540e7c98911002fa877979933b6e76c
danalboteanu <alboteanud@gmail.com>
2019-04-02 23:34:52 +0300

    added widget and focus listener

--- app/src/main/java/com/craiovadata/rfiplayer/MainActivity.kt
+++ app/src/main/java/com/craiovadata/rfiplayer/MainActivity.kt
@@ -1,56 +1,51 @@
 package com.craiovadata.rfiplayer
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import com.craiovadata.rfiplayer.MyService.Companion.getSavedPlayMode
-import com.craiovadata.rfiplayer.MyService.Companion.savePlayMode
 import com.craiovadata.rfiplayer.MyService.Companion.startActionPlay
-import com.craiovadata.rfiplayer.MyService.Companion.startActionTogglePlay
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
-    var menuItemChangeMode: MenuItem? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-        fab.setOnClickListener {
-            startActionTogglePlay(this)
-        }
+//        setSupportActionBar(toolbar)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        menuItemChangeMode = menu?.findItem(R.id.menu_play_mode)
-        val lowModePlay = getSavedPlayMode(this)
-        updateUI(lowModePlay)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when (item?.itemId) {
-            R.id.menu_play_mode -> {
-                var lowModePlay = getSavedPlayMode(this)
-                lowModePlay = !lowModePlay
-                savePlayMode(this, lowModePlay)
-                startActionPlay(this)
-                updateUI(lowModePlay)
+    fun onClick(v: View) {
+        when (v.id) {
+            R.id.buttonPlay48 -> {
+                val playHQ = false
+                startActionPlay(this, playHQ)
+                updateUI(playHQ)
+            }
+            R.id.buttonPlay128 -> {
+                val playHQ = true
+                startActionPlay(this, playHQ)
+                updateUI(playHQ)
+            }
+            R.id.buttonStop -> {
+                val intent = Intent(this, MyService::class.java)
+                stopService(intent)
+                updateUI(null)
             }
         }
-        return super.onOptionsItemSelected(item)
     }
 
-    private fun updateUI(lowModePlay: Boolean) {
-        val txt: String
-        if (lowModePlay) {
-            txt = getString(R.string.text_48_kbps)
-        } else {
-            txt = getString(R.string.text_128_kbps)
+    private fun updateUI(playHQ: Boolean?) {
+        var statusText = "_"
+        if (playHQ == null) {
+            statusText = "_"
+        } else if (!playHQ) {
+            statusText = getString(R.string.text_48_kbps)
+        } else if (playHQ) {
+            statusText = getString(R.string.text_128_kbps)
         }
-        menuItemChangeMode?.title = txt
+        statusTextView.text = statusText
     }
 
 }
--- app/src/main/java/com/craiovadata/rfiplayer/MyService.kt
+++ app/src/main/java/com/craiovadata/rfiplayer/MyService.kt
@@ -8,69 +8,78 @@ import android.os.Build
 import android.os.IBinder
 import androidx.core.app.NotificationCompat
 import androidx.core.app.TaskStackBuilder
+import com.google.android.exoplayer2.C
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.audio.AudioAttributes
 import com.google.android.exoplayer2.source.ExtractorMediaSource
 import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 
+
 private const val ACTION_PLAY = "com.craiovadata.rfiplayer.action.PLAY"
-private const val ACTION_TOGGLE_STATE = "com.craiovadata.rfiplayer.action.TOGGLE_STATE"
+private const val ACTION_TOGGLE_PLAYER = "com.craiovadata.rfiplayer.action.TOGGLE"
 private const val ACTION_STOP = "com.craiovadata.rfiplayer.action.STOP"
+private const val EXTRA_HIGH_QUALITY_PLAY = "com.craiovadata.rfiplayer.extra.HQ"
 
 class MyService : Service() {
 
     private var player: SimpleExoPlayer? = null
+    private var playHQ: Boolean = false
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         val action = intent?.action
-
         when (action) {
             ACTION_PLAY, null -> {
+                playHQ = intent?.getBooleanExtra(EXTRA_HIGH_QUALITY_PLAY, false) ?:false
                 handleActionPlay()
             }
             ACTION_STOP -> {
                 handleActionStop()
             }
-            ACTION_TOGGLE_STATE -> {
-                handleActionToggleState()
+            ACTION_TOGGLE_PLAYER -> {
+                handleActionToggle()
             }
         }
-
         return START_STICKY
     }
 
-    private fun handleActionPlay() {
-        val startLowPlay = getSavedPlayMode(this)
+    private fun handleActionToggle() {
+        if (player==null){
+            handleActionPlay()
+        } else{
+            handleActionStop()
+        }
+    }
 
-        if (startLowPlay) {
-            initializePlayer(getString(R.string.url_48))
-            showNotification(getString(R.string.text_48_kbps))
-        } else {
+    private fun handleActionPlay() {
+        if (playHQ) {
             initializePlayer(getString(R.string.url_128))
             showNotification(getString(R.string.text_128_kbps))
+        } else {
+            initializePlayer(getString(R.string.url_48))
+            showNotification(getString(R.string.text_48_kbps))
         }
     }
 
     private fun handleActionStop() {
         releasePlayer()
-        showNotification("")
-        stopForeground(STOP_FOREGROUND_DETACH)
+//        showNotification("")
+//        stopForeground(STOP_FOREGROUND_DETACH)
+        stopForeground(true)
         stopSelf()
-    }
 
-    private fun handleActionToggleState() {
-        if (player == null) {
-            handleActionPlay()
-        } else {
-            handleActionStop()
-        }
     }
 
     private fun initializePlayer(url: String) {
         if (player == null) {
             player = ExoPlayerFactory.newSimpleInstance(this)
-            player?.setPlayWhenReady(true)
+            player?.playWhenReady = true
+            val audioAttributes = AudioAttributes.Builder()
+                .setUsage(C.USAGE_MEDIA)
+                .setContentType(C.CONTENT_TYPE_MUSIC)
+                .build()
+            player?.setAudioAttributes(audioAttributes, true)
         }
 
         val mediaSource = buildMediaSource(Uri.parse(url))
@@ -102,15 +111,15 @@ class MyService : Service() {
 //            .setAutoCancel(true)
 //            .setOngoing(true)
             .setContentIntent(getPendingIntentToActivity())
-            .addAction(
-                android.R.drawable.ic_media_play,
-                getString(R.string.notif_action_play),
-                getPendingIntentToService(ACTION_PLAY)
-            )
+//            .addAction(
+//                android.R.drawable.ic_media_play,
+//                getString(R.string.notif_action_play),
+//                getPendingIntentToService(ACTION_PLAY)
+//            )
             .addAction(
                 android.R.drawable.ic_media_pause,
                 getString(R.string.notif_action_stop),
-                getPendingIntentToService(ACTION_STOP)
+                buildPendingIntentStop(this)
             )
 
         val notificationManager = getSystemService(Activity.NOTIFICATION_SERVICE) as NotificationManager
@@ -128,12 +137,7 @@ class MyService : Service() {
         startForeground(1, notification)
     }
 
-    private fun getPendingIntentToService(action_name: String): PendingIntent {
-        val intent = Intent(this, MyService::class.java)
-        intent.action = action_name
 
-        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-    }
 
     private fun getPendingIntentToActivity(): PendingIntent? {
         val intentToActivity = Intent(this, MainActivity::class.java)
@@ -157,45 +161,44 @@ class MyService : Service() {
     companion object {
 
         @JvmStatic
-        fun startActionTogglePlay(context: Context) {
-            val intent = Intent(context, MyService::class.java)
-            intent.action = ACTION_TOGGLE_STATE
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                context.startForegroundService(intent)
-            } else {
-                context.startService(intent)
-            }
-        }
-
-        @JvmStatic
-        fun savePlayMode(context: Context, isLowMode: Boolean) {
-            val key = context.getString(R.string.key_pref_low_mode)
-            val editor = context.getSharedPreferences("_", Context.MODE_PRIVATE).edit()
-            editor.putBoolean(key, isLowMode).apply()
-        }
-
-        @JvmStatic
-        fun getSavedPlayMode(context: Context): Boolean {
-            val pref = context.getSharedPreferences("_", Context.MODE_PRIVATE)
-            val key = context.getString(R.string.key_pref_low_mode)
-            return pref.getBoolean(key, true)
-        }
-
-        @JvmStatic
-        fun startActionPlay(context: Context) {
+        fun startActionPlay(context: Context, playHQ: Boolean) {
 
             val intent = Intent(context, MyService::class.java)
             intent.action = ACTION_PLAY
+            intent.putExtra(EXTRA_HIGH_QUALITY_PLAY, playHQ)
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
                 context.startForegroundService(intent)
             } else {
                 context.startService(intent)
             }
+        }
+
+        @JvmStatic
+        fun buildPendingIntentToggle(context: Context): PendingIntent? {
+            val intent = Intent(context, MyService::class.java)
+            intent.action = ACTION_TOGGLE_PLAYER
+//            intent.putExtra(EXTRA_HIGH_QUALITY_PLAY, playHQ)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                return PendingIntent.getForegroundService(context, 0, intent, 0)
+            }
+            return PendingIntent.getService(context, 0, intent, 0)
 
         }
 
+        @JvmStatic
+        fun buildPendingIntentStop(context: Context): PendingIntent? {
+            val intent = Intent(context, MyService::class.java)
+            intent.action = ACTION_STOP
+            return PendingIntent.getService(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+
+
+
     }
+
+
+
 
 }
