95311a83a0a89ea32e2d5ef0aa42451fd9e366b8
Dan ALBOTEANU <alboteanud@gmail.com>
2024-12-18 11:47:28 +0200

    cleanup

--- app/src/main/java/com/craiovadata/rfiplayer/MainActivity.kt
+++ app/src/main/java/com/craiovadata/rfiplayer/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.craiovadata.rfiplayer
 
 import android.Manifest
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -16,11 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (
-            Build.VERSION.SDK_INT >= 33 &&
-            checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) !=
-            PackageManager.PERMISSION_GRANTED
-        ) {
+        if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(arrayOf(Manifest.permission.POST_NOTIFICATIONS), /* requestCode= */ 0)
         }
     }
@@ -38,4 +32,5 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
 }
