9d01f13c0eef73f0e1d723b53ee157557be8c11b
Dan Alboteanu <7892924+alboteanud@users.noreply.github.com>
2026-07-27 16:11:34 +0300

    add Compose

--- app/src/main/java/com/craiovadata/rfiplayer/MainActivity.kt
+++ app/src/main/java/com/craiovadata/rfiplayer/MainActivity.kt
@@ -3,38 +3,125 @@ package com.craiovadata.rfiplayer
 import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.View
-import androidx.appcompat.app.AppCompatActivity
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.craiovadata.rfiplayer.AudioService.Companion.startActionPlay
 import com.craiovadata.rfiplayer.AudioService.Companion.startActionStop
+import com.craiovadata.rfiplayer.ui.theme.RFIplayerTheme
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         if (checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(arrayOf(Manifest.permission.POST_NOTIFICATIONS), /* requestCode= */ 0)
         }
-    }
 
-    fun onClick(v: View) {
-        when (v.id) {
-            R.id.buttonPlayRfi -> {
-                startActionPlay(this, "http://asculta.rfi.ro:9128/live.mp3")
-            }
-            R.id.buttonItzyBitzy -> {
-                startActionPlay(this, "http://live.itsybitsy.ro:8000/itsybitsy")
-            }
-            R.id.buttonFranceInter -> {
-                startActionPlay(this, "http://icecast.radiofrance.fr/franceinter-hifi.aac")
-                // "http://icecast.radiofrance.fr/franceinter-midfi.mp3" - data saver
-            }
-            R.id.buttonStop -> {
-                startActionStop(this)
+        setContent {
+            RFIplayerTheme {
+                MainScreen(
+                    onPlayRfi = { startActionPlay(this, "http://asculta.rfi.ro:9128/live.mp3") },
+                    onPlayItsyBitsy = { startActionPlay(this, "http://live.itsybitsy.ro:8000/itsybitsy") },
+                    onPlayFranceInter = { startActionPlay(this, "http://icecast.radiofrance.fr/franceinter-hifi.aac") },
+                    onStop = { startActionStop(this) }
+                )
             }
         }
     }
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(
+    onPlayRfi: () -> Unit,
+    onPlayItsyBitsy: () -> Unit,
+    onPlayFranceInter: () -> Unit,
+    onStop: () -> Unit
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(stringResource(R.string.app_name)) },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Spacer(modifier = Modifier.weight(1f))
+            
+            StationButton(
+                text = stringResource(R.string.rfi),
+                onClick = onPlayRfi
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            StationButton(
+                text = stringResource(R.string.inter),
+                onClick = onPlayFranceInter
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            StationButton(
+                text = stringResource(R.string.itzy_bitzy),
+                onClick = onPlayItsyBitsy
+            )
+
+            Spacer(modifier = Modifier.weight(1f))
+
+            IconButton(
+                onClick = onStop,
+                modifier = Modifier.size(75.dp)
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_stop),
+                    contentDescription = stringResource(R.string.stop),
+                    modifier = Modifier.fillMaxSize(),
+                    tint = MaterialTheme.colorScheme.primary
+                )
+            }
+
+            Spacer(modifier = Modifier.weight(1f))
+        }
+    }
+}
+
+@Composable
+fun StationButton(text: String, onClick: () -> Unit) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .width(140.dp)
+            .height(75.dp),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Text(text)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreview() {
+    RFIplayerTheme {
+        MainScreen({}, {}, {}, {})
+    }
 }
---
+++ app/src/main/java/com/craiovadata/rfiplayer/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.craiovadata.rfiplayer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val RFIRed = Color(0xFFD00009)
+val RFIRedDark = Color(0xFFA00009)
+val RFIAccent = Color(0xFFFFC107)
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
